f4a50ff9993743502a70f27d0955066d6ece9919
Florian <florian@ROGER>
2024-02-14 10:28:09 +0100

    first commit

---
+++ api/models/user_m.js
@@ -0,0 +1,27 @@
+/************************************/
+/*** Import des modules nécessaires */
+const { DataTypes } = require('sequelize')
+
+/*******************************/
+/*** Définition du modèle User */
+module.exports = (sequelize) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER(10),
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nom:{
+            type: DataTypes.STRING(100),
+            defaultValue: '',
+            allowNull: false
+        },
+        prenom:{
+            type: DataTypes.STRING(100),
+            defaultValue: '',
+            allowNull: false
+        }
+    })    
+
+    return User
+}
